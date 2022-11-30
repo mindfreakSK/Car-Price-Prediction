3fb047c7fd8d2c73eb7ca58f72e3527ab53d6d74
mindfreakSK <shazzshaikh3@gmail.com>
2022-11-30 17:41:34 +0530

    Gunicorn configuration changes

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,3 +2,24 @@
 
 source env/bin/activate
 cd /var/lib/jenkins/workspace/Python\ CI-CD
+
+echo "Setup is going on.."
+
+echo " Setup is completed"
+
+sudo cp -rf gunicorn.socket /etc/systemd/system
+sudo cp -rf gunicorn.service /etc/systemd/system
+
+echo "$USER"
+echo "PWD"
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+
+echo "Gunicorn has been started"
+
+sudo systemctl status gunicorn
+sudo systemctl restart gunicorn
+
+sudo systemctl status gunicorn
